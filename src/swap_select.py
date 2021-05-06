508129aaaea67516eea70bcf1c4ed26811ea4cc2
Will <williampayne23@gmail.com>
2021-05-06 13:11:19 +0100

    Hadn't fully removed follow focus

--- src/swap_select.py
+++ src/swap_select.py
@@ -1,26 +1,26 @@
 import os, json, sys
 from yabai_tools import *
 
-def initiate(direction, follow_focus=True):
+def initiate(direction):
     next_bordered = id_in_direction(get_main_id(), direction)
     if (next_bordered == None):
-        send_to_display(direction, follow_focus=follow_focus)
+        send_to_display(direction)
         return
     toggle_bordered(next_bordered)
     
 
-def move(direction, current_id, follow_focus=True):
+def move(direction, current_id):
     next_bordered = id_in_direction(current_id, direction)
     if(next_bordered == None):
-        send_to_display(direction, follow_focus=follow_focus)
+        send_to_display(direction)
         toggle_bordered(current_id)
         return
     toggle_bordered(next_bordered)
     toggle_bordered(current_id)
 
-def end(current_id, follow_focus=True):
+def end(current_id):
     toggle_bordered(current_id)
-    swap_to(current_id, follow_focus=follow_focus)
+    swap_to(current_id)
 
 def get_bordered():
     output = get_windows()
@@ -30,17 +30,17 @@ def get_bordered():
     else:
         return -1
 
-def swap_select(direction, follow_focus=True, key_mod=False):
+def swap_select(direction, key_mod=False):
     bordered = get_bordered()
     if(direction == None or direction == 'swap'):
         if (bordered != -1):
-            end(bordered, follow_focus)
+            end(bordered)
     else:
         if (bordered == -1):
-            initiate(direction, follow_focus=follow_focus)
+            initiate(direction)
             if(key_mod):
                 dirname = os.path.dirname(__file__)
                 filename = os.path.join(dirname, 'yabaiEndSwitch.scpt')
                 os.system('osascript ' + filename)
         else:
-            move(direction, bordered, follow_focus=follow_focus) 
+            move(direction, bordered) 
